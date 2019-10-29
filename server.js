a46e009187fe8dc5eb519c4837205c46cc6ef2ab
Josué Cornejo <josue.cornejo@wizeline.com>
2019-10-29 15:50:59 -0600

    Create endpoint admins/notificate

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mysql = require('mysql');
 const dotenv = require('dotenv').config();
 
+const app = express();
+app.use(express.json())
+
 const connection = mysql.createConnection({
   host: process.env.HOST,
   port: process.env.PORT,
@@ -9,21 +12,31 @@ const connection = mysql.createConnection({
   password: process.env.PASSWORD,
   database: process.env.DATABASE
 });
-connection.connect();
 
-const app = express();
-
-app.get('/', function (req, res) {
-  return res.send({ error: true, message: 'hello' })
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
 });
 
-app.get('/users', function (req, res) {
-  connection.query('SELECT * from user', function (err, result) {
+app.get('/', (req, res) => res.send({ message: 'hello' }));
+
+app.get('/codes', (req, res) => {
+  connection.query('SELECT * from error', (err, result) => {
     if (err) throw err
-    return res.send({ error: false, data: result, message: 'users list.' });
+    return res.send({ data: result });
   })
 });
 
-app.listen(8080, function() {
-  console.log("Server running on port 8080");
+app.post('/admins/notificate', (req, res) => {
+  const sql = `SELECT user.email, user.first_name, user.last_name FROM error_user INNER JOIN user ON user.id = error_user.userId WHERE errorId = '${req.body.codeId}'`;
+  connection.query(sql, (err, result) => {
+    if (err) throw err
+    return res.send({ data: { code: { uuid: req.body.codeId }, admins: result } });
+  });
 });
+
+app.listen(8080, () => console.log("Server running on port 8080"));

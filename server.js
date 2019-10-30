656588ef8297ab7eb72aa893df2eb40d152d7144
Josué Cornejo <josue.cornejo@wizeline.com>
2019-10-30 10:05:54 -0600

    Add Mailtrap to send email notification

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
+const nodemailer = require('nodemailer');
 
 const app = express();
 app.use(express.json())
@@ -13,6 +14,15 @@ const connection = mysql.createConnection({
   user: process.env.USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE
+});
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAILTRAP_SMTP_USERNAME,
+    pass: process.env.MAILTRAP_SMTP_PASSWORD
+  }
 });
 
 connection.connect((err) => {
@@ -38,9 +48,33 @@ app.post('/admins/notificate', (req, res) => {
   const sql = `SELECT user.email, user.first_name, user.last_name FROM error_user INNER JOIN user ON user.id = error_user.userId WHERE errorId = '${uuid}'`;
   connection.query(sql, (err, result) => {
     if (err) throw err
-    console.log(result);
-    return res.send({ data: { code: { uuid }, admins: result } });
+
+    const emails = result.map(user => user.email).join(', ');
+    const mailOptions = {
+      from: '"Test Server" <support@mosaicui.com>',
+      to: emails,
+      subject: "Mosaic UI - Error Support",
+      text: "This is an mosaic UI email test"
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if(err){
+        console.log(err);
+        next(err)
+      }
+
+      console.log('Message sent: %s', info.messageId);
+      return res.send({ message: "Email successfully sent." });
+    });
   });
 });
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    error: {
+      message: err.message || "Internal Server Error"
+    }
+  })
+})
 
 app.listen(8080, () => console.log("Server running on port 8080"));

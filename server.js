240f6daad388bf71b1b37aedbec735cb44ac516a
Josué Cornejo <josue.cornejo@wizeline.com>
2019-10-29 18:28:23 -0600

    Implements CORS library

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 app.use(express.json())
+app.use(cors());
 
 const connection = mysql.createConnection({
   host: process.env.HOST,
@@ -32,11 +34,12 @@ app.get('/codes', (req, res) => {
 });
 
 app.post('/admins/notificate', (req, res) => {
-  const uuid = req.body.codeId || req.body.data.select;
+  const uuid = req.body.request.data.select;
   const sql = `SELECT user.email, user.first_name, user.last_name FROM error_user INNER JOIN user ON user.id = error_user.userId WHERE errorId = '${uuid}'`;
   connection.query(sql, (err, result) => {
     if (err) throw err
-    return res.send({ data: { code: { uuid: req.body.codeId }, admins: result } });
+    console.log(result);
+    return res.send({ data: { code: { uuid }, admins: result } });
   });
 });
 

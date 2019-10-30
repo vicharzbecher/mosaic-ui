3e7a4fef337140bc89164a868ccf3234931ef30c
Victor Harzbecher <victor.harzbecher@dowjones.com>
2019-10-30 12:00:18 -0600

    Adding sass styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 //import logo from './logo.svg';
 import {Form} from 'react-formio';
+import './styles.scss'
 import './App.css';
 require('dotenv').config();
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        Form render example
+        Form render example (source: form.io)
       </header>
-      <div className="panel panel-default">
+      <br/>
+      <br/>
+      <div className="panel panel-default"  style={{width: 300, margin: 'auto'}}>
         <div className="panel-heding">
           <div className="panel-title">
-            Form
+            CNS test Form
           </div>
         </div>
+        
         <Form src="https://osnodsjcwhrwjsk.form.io/dropdown"></Form>
       </div>
     </div>

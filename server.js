030dfe629685aa2526d48053eb7909f937e5e417
Josué Cornejo <josue.cornejo@wizeline.com>
2019-11-05 12:55:04 -0600

    Restore to parse content-type json

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 const pool = require('./database');
-const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.text());
-app.use(bodyParser.json());
+app.use(express.json())
 app.use(cors());
 
 const transporter = nodemailer.createTransport({
